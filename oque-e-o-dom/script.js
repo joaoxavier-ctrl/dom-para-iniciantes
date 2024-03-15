5ad6f5589a4bb0240cfd649713715ea4fd3ecb49
JoaoXavierWin <joao.xavier21@gmail.com>
2024-03-14 23:51:12 -0300

    oque-e-o-dom

---
+++ oque-e-o-dom/script.js
@@ -0,0 +1,16 @@
+// Retorne o url da página atual utilizando o objeto window
+const href = window.location.href;
+alert(href);
+
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const ativo = document.querySelector('.ativo');
+
+// Retorne a linguagem do navegador
+alert(window.navigator.language);
+
+// Retorne a largura da janela 
+const windowWidth = window.innerWidth;
+
+alert(windowWidth);
