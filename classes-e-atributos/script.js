17f6a09c48ee5c22e5699338607dc5ba31d0ed40
JoaoXavierWin <joao.xavier21@gmail.com>
2024-03-18 19:30:32 -0300

    classes-e-atributos-finalizada

--- classes-e-atributos/script.js
+++ classes-e-atributos/script.js
@@ -1,17 +1,30 @@
 // Adicione a classe ativo a todos os itens do menu
-const menu = document.querySelector('.menu');
+const menu = document.querySelectorAll('.menu a');
 
-menu.classList.add('ativo');
+menu.forEach((item) => {
+    item.classList.add('ativo');
+});
 
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
 
-menu.classList.remove('ativo');
+menu.forEach((item) => {
+    item.classList.remove('ativo');
+});
+
+menu[0].classList.add('ativo');
 
 // Verifique se as imagens possuem o atributo alt
 
-const img = document.querySelector('img');
+const imgs = document.querySelectorAll('img');
 
-img.hasAttribute('alt');
+imgs.forEach((img) => {
+    const possuiAtributo = img.hasAttribute('alt');
+    console.log(possuiAtributo);
+});
 
 // Modifique o href do link externo no menu
 
+const linkInterno = document.querySelector('[href^="https://github.com/joaoxavier-ctrl"');
+linkInterno.setAttribute('href', 'google.com');
+
+console.log(linkInterno);
